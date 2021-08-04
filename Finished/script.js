203fa9f65bcab46b7dc92c5b780fc2ed89fa3234
Ziratsu <enzo.ustariz@hotmail.fr>
2021-08-04 16:36:36 +0200

    first commit

---
+++ Finished/script.js
@@ -0,0 +1,18 @@
+const goTopBtn = document.querySelector('.go-top-btn');
+
+window.addEventListener('scroll', checkHeight)
+
+function checkHeight(){
+  if(window.scrollY > 200) {
+    goTopBtn.style.display = "flex"
+  } else {
+    goTopBtn.style.display = "none"
+  }
+}
+
+goTopBtn.addEventListener('click', () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  })
+})
